bae83229dc0d5ac03e2e9638464bf8e5430ad063
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-08-15 22:25:24 +0530

    made about

---
+++ src/components/About/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HEADER_H = 60;
+
+const About = () => {
+  return (
+    <div 
+    className='min-h-full overflow-auto'
+    >
+      
+    </div>
+  )
+}
+
+export default About;
