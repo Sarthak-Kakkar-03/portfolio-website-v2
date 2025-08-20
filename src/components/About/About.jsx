543f3b3d7e9f715f4ea1276f925eab7343d04b53
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-08-20 16:37:16 +0530

    about structure finished - cards

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,40 +1,160 @@
-import React from 'react'
-import paragraphs from "/src/data/about.json"
-
+import React from "react";
+import paragraphs from "/src/data/about.json";
 
 const About = () => {
   return (
-    <div className=" flex flex-col-reverse md:grid grid-cols-[70%_30%] min-h-full min-w-full p-2 gap-3">
-      <div className='flex flex-col justify-center items-center 
-      gap-2 flex-wrap 
-      md:shadow rounded-4xl bg-white p-5
-      transition-all duration-300 ease-in-out
-      transform-gpu hover:scale-[1.02] hover:translate-y-1 hover:shadow-lg
-      '>
-        <h1 className='text-xl md:text-6xl font-extrabold hover:text-highlight transition-colors duration-500'>Sarthak Kakkar</h1>
-        <p className='font-extralight text-base md:text-xl text-center md:text-left '>I am a software engineer with a focus on building end-to-end solutions that bridge specialized hardware systems and AI-driven workflows. My work has included developing a system certified for medical research on humans and creating an AI-powered code generation and auto-correction MVP that was presented to several companies. I have also contributed to academia as a teaching assistant for C++ and as a published author at the International Conference on Learning Representations (ICLR).</p>
-        <div className='flex flex-col md:flex-row justify-center items-center md:justify-start w-full mt-2 gap-1'>
-          <div className='hover:text-highlight'>
-              <svg className='fill-current w-15 h-15' viewBox="0 0 14 14" role="img" focusable="false" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"><path d="m 12.499079,12.25525 c 0.0968,0 0.188377,-0.0436 0.249339,-0.11884 0.06096,-0.0752 0.08473,-0.17385 0.06473,-0.26853 l -0.202146,-0.95662 c 0.115125,-0.11137 0.187491,-0.26686 0.187491,-0.43975 0,-0.182 -0.08106,-0.34343 -0.206876,-0.45558 l 0,-3.32202 -0.810333,0.50146 0,2.82056 c -0.125815,0.11215 -0.2069,0.27358 -0.2069,0.45558 0,0.17291 0.07239,0.32841 0.187515,0.43975 l -0.20217,0.95662 c -0.02,0.0947 0.0038,0.19335 0.06473,0.26853 0.06096,0.0752 0.152539,0.11884 0.249339,0.11884 l 0.625281,0 z M 12.773741,4.75539 7.5021019,1.49209 C 7.3477151,1.39699 7.1736728,1.34925 6.9996305,1.34925 c -0.1740423,0 -0.3482077,0.0477 -0.5016586,0.14284 l -5.271713,3.2633 C 1.0854931,4.84249 0.99999905,4.99633 0.99999905,5.1619 c 0,0.1656 0.085494,0.31949 0.22625985,0.40673 l 5.2716883,3.26333 c 0.153451,0.0952 0.3276163,0.14284 0.5016586,0.14284 0.1740423,0 0.3481092,-0.0477 0.5024714,-0.14284 L 12.773741,5.56863 c 0.140766,-0.0872 0.22626,-0.24113 0.22626,-0.40673 0,-0.16557 -0.08549,-0.31946 -0.22626,-0.40651 z M 6.9996059,9.78508 c -0.3283798,0 -0.6488777,-0.0912 -0.928242,-0.26411 l -3.0750017,-1.90368 0,3.27796 c 0,0.97016 1.7931578,1.7555 4.0032436,1.7555 2.2108742,0 4.0038842,-0.78536 4.0038842,-1.7555 l 0,-3.27796 -3.0748786,1.90368 C 7.6492472,9.69388 7.3279857,9.78508 6.9996059,9.78508 Z"/></svg>
+    <div className="text-matte">
+      <div className=" flex flex-col-reverse md:grid grid-cols-[75%_25%] min-h-full min-w-full p-2 gap-3">
+        <div
+          className="flex flex-col justify-center items-center 
+        gap-2 flex-wrap 
+        md:shadow rounded-4xl bg-white p-5
+        transition-all duration-300 ease-in-out
+        transform-gpu hover:scale-[1.02] hover:translate-y-1 hover:shadow-lg
+        group
+        "
+        >
+          <h1 className="text-xl md:text-6xl font-extrabold group-hover:text-highlight transition-colors duration-500">
+            Sarthak Kakkar
+          </h1>
+          <p className="font-extralight text-base md:text-xl text-center md:text-left ">
+            I am a software engineer with a focus on building end-to-end
+            solutions that bridge specialized hardware systems and AI-driven
+            workflows. My work has included developing a system certified for
+            medical research on humans and creating an AI-powered code
+            generation and auto-correction MVP that was presented to several
+            companies. I have also contributed to academia as a teaching
+            assistant for C++ and as a published author at the International
+            Conference on Learning Representations (ICLR).
+          </p>
+          <div className="flex flex-col md:flex-row justify-center items-center md:justify-start w-full mt-2 gap-1">
+            <div className="group-hover:text-highlight duration-500">
+              <svg
+                className="fill-current w-15 h-15"
+                viewBox="0 0 14 14"
+                role="img"
+                focusable="false"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="m 12.499079,12.25525 c 0.0968,0 0.188377,-0.0436 0.249339,-0.11884 0.06096,-0.0752 0.08473,-0.17385 0.06473,-0.26853 l -0.202146,-0.95662 c 0.115125,-0.11137 0.187491,-0.26686 0.187491,-0.43975 0,-0.182 -0.08106,-0.34343 -0.206876,-0.45558 l 0,-3.32202 -0.810333,0.50146 0,2.82056 c -0.125815,0.11215 -0.2069,0.27358 -0.2069,0.45558 0,0.17291 0.07239,0.32841 0.187515,0.43975 l -0.20217,0.95662 c -0.02,0.0947 0.0038,0.19335 0.06473,0.26853 0.06096,0.0752 0.152539,0.11884 0.249339,0.11884 l 0.625281,0 z M 12.773741,4.75539 7.5021019,1.49209 C 7.3477151,1.39699 7.1736728,1.34925 6.9996305,1.34925 c -0.1740423,0 -0.3482077,0.0477 -0.5016586,0.14284 l -5.271713,3.2633 C 1.0854931,4.84249 0.99999905,4.99633 0.99999905,5.1619 c 0,0.1656 0.085494,0.31949 0.22625985,0.40673 l 5.2716883,3.26333 c 0.153451,0.0952 0.3276163,0.14284 0.5016586,0.14284 0.1740423,0 0.3481092,-0.0477 0.5024714,-0.14284 L 12.773741,5.56863 c 0.140766,-0.0872 0.22626,-0.24113 0.22626,-0.40673 0,-0.16557 -0.08549,-0.31946 -0.22626,-0.40651 z M 6.9996059,9.78508 c -0.3283798,0 -0.6488777,-0.0912 -0.928242,-0.26411 l -3.0750017,-1.90368 0,3.27796 c 0,0.97016 1.7931578,1.7555 4.0032436,1.7555 2.2108742,0 4.0038842,-0.78536 4.0038842,-1.7555 l 0,-3.27796 -3.0748786,1.90368 C 7.6492472,9.69388 7.3279857,9.78508 6.9996059,9.78508 Z" />
+              </svg>
+            </div>
+            <div className="flex flex-col text-center md:text-left group-hover:text-highlight">
+              <a
+                href="https://www.northeastern.edu/"
+                target="_blank"
+                className="font-bold"
+              >
+                Northeastern University
+              </a>
+              <h3>Bachelor of Science in Computer Science (BSCS)</h3>
+              <p>Class of 2026</p>
+            </div>
           </div>
-          <div className='flex flex-col text-center md:text-left'>
-            <a href="https://www.northeastern.edu/" target='_blank' className='hover:text-highlight font-bold'>Northeastern University</a>
-            <h3>Bachelor of Science in Computer Science (BSCS)</h3>
-            <p>Class of 2026</p>
+        </div>
+        <div
+          className="h-full md:bg-white rounded-4xl p-3 md:flex flex-col gap-5
+        justify-center items-center md:shadow
+        transition-all duration-300 ease-in-out
+        transform-gpu hover:scale-[1.02] hover:translate-y-1 hover:shadow-lg
+        group"
+        >
+          <div className="w-48 h-48 md:w-72 md:h-72 overflow-hidden rounded-full mx-auto">
+            <img
+              src="/src/assets/about_img_rect.jpg"
+              alt="Me"
+              className="w-full h-full object-cover"
+            />
           </div>
-          
+          <div className="hidden md:flex flex-1 w-full justify-evenly p-1 mt-3">
+            <a
+              href="mailto:kakkar.sar@gmail.com"
+              target="_blank"
+              className="transition-all hover:scale-[1.35] duration-300 hover:text-highlight hover:translate-y-1"
+            >
+              <svg
+                className="stroke-current"
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M4 9.00005L10.2 13.65C11.2667 14.45 12.7333 14.45 13.8 13.65L20 9"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+                <path
+                  d="M3 9.17681C3 8.45047 3.39378 7.78123 4.02871 7.42849L11.0287 3.5396C11.6328 3.20402 12.3672 3.20402 12.9713 3.5396L19.9713 7.42849C20.6062 7.78123 21 8.45047 21 9.17681V17C21 18.1046 20.1046 19 19 19H5C3.89543 19 3 18.1046 3 17V9.17681Z"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                />
+              </svg>
+            </a>
+            <a
+              href="https://github.com/Sarthak-Kakkar-03"
+              target="_blank"
+              className="transition-all hover:scale-[1.35] duration-300 hover:text-highlight hover:translate-y-1"
+            >
+              <svg
+                className="fill-current stroke-current"
+                width="30px"
+                height="30px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M4.0744 2.9938C4.13263 1.96371 4.37869 1.51577 5.08432 1.15606C5.84357 0.768899 7.04106 0.949072 8.45014 1.66261C9.05706 1.97009 9.11886 1.97635 10.1825 1.83998C11.5963 1.65865 13.4164 1.65929 14.7213 1.84164C15.7081 1.97954 15.7729 1.97265 16.3813 1.66453C18.3814 0.651679 19.9605 0.71795 20.5323 1.8387C20.8177 2.39812 20.8707 3.84971 20.6494 5.04695C20.5267 5.71069 20.5397 5.79356 20.8353 6.22912C22.915 9.29385 21.4165 14.2616 17.8528 16.1155C17.5801 16.2574 17.3503 16.3452 17.163 16.4167C16.5879 16.6363 16.4133 16.703 16.6247 17.7138C16.7265 18.2 16.8491 19.4088 16.8973 20.4002C16.9844 22.1922 16.9831 22.2047 16.6688 22.5703C16.241 23.0676 15.6244 23.076 15.2066 22.5902C14.9341 22.2734 14.9075 22.1238 14.9075 20.9015C14.9075 19.0952 14.7095 17.8946 14.2417 16.8658C13.6854 15.6415 14.0978 15.185 15.37 14.9114C17.1383 14.531 18.5194 13.4397 19.2892 11.8146C20.0211 10.2698 20.1314 8.13501 18.8082 6.83668C18.4319 6.3895 18.4057 5.98446 18.6744 4.76309C18.7748 4.3066 18.859 3.71768 18.8615 3.45425C18.8653 3.03823 18.8274 2.97541 18.5719 2.97541C18.4102 2.97541 17.7924 3.21062 17.1992 3.49805L16.2524 3.95695C16.1663 3.99866 16.07 4.0147 15.975 4.0038C13.5675 3.72746 11.2799 3.72319 8.86062 4.00488C8.76526 4.01598 8.66853 3.99994 8.58215 3.95802L7.63585 3.49882C7.04259 3.21087 6.42482 2.97541 6.26317 2.97541C5.88941 2.97541 5.88379 3.25135 6.22447 4.89078C6.43258 5.89203 6.57262 6.11513 5.97101 6.91572C5.06925 8.11576 4.844 9.60592 5.32757 11.1716C5.93704 13.1446 7.4295 14.4775 9.52773 14.9222C10.7926 15.1903 11.1232 15.5401 10.6402 16.9905C10.26 18.1319 10.0196 18.4261 9.46707 18.4261C8.72365 18.4261 8.25796 17.7821 8.51424 17.1082C8.62712 16.8112 8.59354 16.7795 7.89711 16.5255C5.77117 15.7504 4.14514 14.0131 3.40172 11.7223C2.82711 9.95184 3.07994 7.64739 4.00175 6.25453C4.31561 5.78028 4.32047 5.74006 4.174 4.83217C4.09113 4.31822 4.04631 3.49103 4.0744 2.9938Z" />
+                <path d="M3.33203 15.9454C3.02568 15.4859 2.40481 15.3617 1.94528 15.6681C1.48576 15.9744 1.36158 16.5953 1.66793 17.0548C1.8941 17.3941 2.16467 17.6728 2.39444 17.9025C2.4368 17.9449 2.47796 17.9858 2.51815 18.0257C2.71062 18.2169 2.88056 18.3857 3.05124 18.5861C3.42875 19.0292 3.80536 19.626 4.0194 20.6962C4.11474 21.1729 4.45739 21.4297 4.64725 21.5419C4.85315 21.6635 5.07812 21.7352 5.26325 21.7819C5.64196 21.8774 6.10169 21.927 6.53799 21.9559C7.01695 21.9877 7.53592 21.998 7.99999 22.0008C8.00033 22.5527 8.44791 23.0001 8.99998 23.0001C9.55227 23.0001 9.99998 22.5524 9.99998 22.0001V21.0001C9.99998 20.4478 9.55227 20.0001 8.99998 20.0001C8.90571 20.0001 8.80372 20.0004 8.69569 20.0008C8.10883 20.0026 7.34388 20.0049 6.67018 19.9603C6.34531 19.9388 6.07825 19.9083 5.88241 19.871C5.58083 18.6871 5.09362 17.8994 4.57373 17.2891C4.34391 17.0194 4.10593 16.7834 3.91236 16.5914C3.87612 16.5555 3.84144 16.5211 3.80865 16.4883C3.5853 16.265 3.4392 16.1062 3.33203 15.9454Z" />
+              </svg>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/sarthakkakkar03"
+              target="_blank"
+              className="transition-all hover:scale-[1.35] duration-300 hover:text-highlight hover:translate-y-1"
+            >
+              <svg
+                className="fill-current line-current"
+                width="30px"
+                height="30px"
+                viewBox="0 0 20 20"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+              >
+                <title>linkedin [#161]</title>
+                <desc>Created with Sketch.</desc>
+                <defs></defs>
+                <g id="Page-1" strokeWidth="1" fillRule="evenodd">
+                  <g
+                    id="Dribbble-Light-Preview"
+                    transform="translate(-180.000000, -7479.000000)"
+                  >
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                      <path
+                        d="M144,7339 L140,7339 L140,7332.001 C140,7330.081 139.153,7329.01 137.634,7329.01 C135.981,7329.01 135,7330.126 135,7332.001 L135,7339 L131,7339 L131,7326 L135,7326 L135,7327.462 C135,7327.462 136.255,7325.26 139.083,7325.26 C141.912,7325.26 144,7326.986 144,7330.558 L144,7339 L144,7339 Z M126.442,7323.921 C125.093,7323.921 124,7322.819 124,7321.46 C124,7320.102 125.093,7319 126.442,7319 C127.79,7319 128.883,7320.102 128.883,7321.46 C128.884,7322.819 127.79,7323.921 126.442,7323.921 L126.442,7323.921 Z M124,7339 L129,7339 L129,7326 L124,7326 L124,7339 Z"
+                        id="linkedin-[#161]"
+                      ></path>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </a>
+          </div>
+          <div className="w-full hidden md:flex justify-center items-center">
+            <a href="/Sarthak_Kakkar_resume.pdf" download="Sarthak_Kakkar_resume.pdf"  className="
+            bg-surface p-2 px-6 font-bold rounded shadow
+            transition-all hover:scale-[1.2] duration-300 hover:shadow-bg
+            hover:cursor-pointer border-neutral-900 hover:text-highlight hover:border-highlight border-2"
+            >DOWNLOAD CV</a>
+          </div>
         </div>
       </div>
-      <div className="w-48 h-48 md:w-72 md:h-72 overflow-hidden rounded-full mx-auto">
-        <img
-          src="/src/assets/about_img_rect.jpg"
-          alt="Me"
-          className="w-full h-full object-cover transition-transform duration-300 hover:scale-105"
-        />
-      </div>
-</div>
-
-  )
-}
+    </div>
+  );
+};
 
 export default About;
