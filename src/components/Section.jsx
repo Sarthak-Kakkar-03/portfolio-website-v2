0e5a56a906848a6353aee1e41fc4a5fdd26eaa57
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-08-21 11:01:32 +0530

    fixed scroll bug

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -5,7 +5,7 @@ export default function Section({ id, className = '', children }) {
     <section
       id={id}
       data-fullpage
-      className={`bg-surface min-h-[calc(100dvh-4rem)] px-2 py-4 md:px-10 md:py-12 md:scroll-mt-16 ${className}`}
+      className={`bg-surface min-h-[calc(100dvh-4rem)] px-2 py-4 md:px-10 md:py-12 ${className}`}
     >
       {children}
     </section>
