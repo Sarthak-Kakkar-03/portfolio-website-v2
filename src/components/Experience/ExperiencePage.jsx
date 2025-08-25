14b4c9b336ac1700b24aa26080501f34fa49512d
Sarthak-Kakkar-03 <sarthakkakkar2021@gmail.com>
2025-08-25 13:16:32 +0530

    recommendations are visible now

--- src/components/Experience/ExperiencePage.jsx
+++ src/components/Experience/ExperiencePage.jsx
@@ -45,14 +45,14 @@ const ExperiencePage = () => {
         <PageSection title={"Responsibilities"}>
           <ul className="list-disc list-inside">
             {exp.responsibilities.map((responsibility, idx) => {
-              return <li key={idx}>{responsibility}</li>;
+              return <li key={`resp-${idx}`}>{responsibility}</li>;
             })}
           </ul>
         </PageSection>
         <PageSection title={"Key Achievements"}>
           <ul className="list-disc list-inside">
             {exp.achievements.map((achievement, idx) => {
-              return <li key={idx}>{achievement}</li>;
+              return <li key={`ach-${idx}`}>{achievement}</li>;
             })}
           </ul>
         </PageSection>
@@ -62,7 +62,7 @@ const ExperiencePage = () => {
               //console.log(tech);
               return (
                 <div
-                  key={idx}
+                  key={`tech-${idx}`}
                   className="p-2 bg-surface rounded-xl shadow font-semibold text-sm"
                 >
                   {tech}
@@ -71,6 +71,67 @@ const ExperiencePage = () => {
             })}
           </div>
         </PageSection>
+        {exp.recommendations.length !== 0 && (
+          <PageSection title="Recommendations">
+            {exp.recommendations.map((rec, idx) => (
+              <div
+                key={`rec-${idx}`}
+                className="flex flex-col md:flex-row gap-5 md:items-start mb-8"
+              >
+                <div className="md:w-1/3 md:flex-shrink-0 flex flex-col  gap-2 items-center group">
+                  {rec.image ? (
+                    <img
+                      src={rec.image}
+                      alt={rec.name}
+                      className="w-24 h-24 md:w-28 md:h-28 rounded-xl object-cover bg-surface transition-all hover:-translate-y-1 hover:scale-[1.05]"
+                    />
+                  ) : (
+                    <div className="w-24 h-24 md:w-28 md:h-28 rounded-xl bg-surface" />
+                  )}
+                  <h2 className="text-lg font-semibold">{rec.name}</h2>
+                  <h2 className="text-lg">{rec.relation}</h2>
+                  <a
+              href={rec.linkedIn}
+              target="_blank"
+              className="transition-all hover:scale-[1.35] duration-300 group-hover:text-highlight hover:translate-y-1"
+            >
+              <svg
+                className="fill-current line-current"
+                width="25px"
+                height="25px"
+                viewBox="0 0 20 20"
+                version="1.1"
+                xmlns="http://www.w3.org/2000/svg"
+                xmlnsXlink="http://www.w3.org/1999/xlink"
+              >
+                <title>linkedin [#161]</title>
+                <desc>Created with Sketch.</desc>
+                <defs></defs>
+                <g id="Page-1" strokeWidth="1" fillRule="evenodd">
+                  <g
+                    id="Dribbble-Light-Preview"
+                    transform="translate(-180.000000, -7479.000000)"
+                  >
+                    <g id="icons" transform="translate(56.000000, 160.000000)">
+                      <path
+                        d="M144,7339 L140,7339 L140,7332.001 C140,7330.081 139.153,7329.01 137.634,7329.01 C135.981,7329.01 135,7330.126 135,7332.001 L135,7339 L131,7339 L131,7326 L135,7326 L135,7327.462 C135,7327.462 136.255,7325.26 139.083,7325.26 C141.912,7325.26 144,7326.986 144,7330.558 L144,7339 L144,7339 Z M126.442,7323.921 C125.093,7323.921 124,7322.819 124,7321.46 C124,7320.102 125.093,7319 126.442,7319 C127.79,7319 128.883,7320.102 128.883,7321.46 C128.884,7322.819 127.79,7323.921 126.442,7323.921 L126.442,7323.921 Z M124,7339 L129,7339 L129,7326 L124,7326 L124,7339 Z"
+                        id="linkedin-[#161]"
+                      ></path>
+                    </g>
+                  </g>
+                </g>
+              </svg>
+            </a>
+                </div>
+
+                {/* Right column */}
+                <div className="md:w-2/3 md:min-w-0 leading-relaxed bg-surface p-2 rounded text-center">
+                  {rec.content}
+                </div>
+              </div>
+            ))}
+          </PageSection>
+        )}
       </div>
     </div>
   );
